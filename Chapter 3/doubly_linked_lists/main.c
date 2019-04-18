7a0f183f37e30df2b5d7e3ecf848d40dad0f4a03
DarkUlixes <reiarifi@gmail.com>
2019-04-18 10:37:07 +0200

    Test of doubly linked list

--- Chapter 3/doubly_linked_lists/main.c
+++ Chapter 3/doubly_linked_lists/main.c
@@ -3,5 +3,14 @@
 
 int main(int argc, char* argv[])
 {
-	list* l = MakeListLeft(3, MakeListLeft(1, MakeListRight(5, MakeListRight(2, MakeListLeft(4, EmptyList())))));
+	//list* l = MakeListLeft(3, MakeListLeft(1, MakeListRight(5, MakeListRight(2, MakeListLeft(4, EmptyList())))));
+
+	puts("TEST MakeListLeft");
+	list* l = MakeListLeft(1, MakeListLeft(2, MakeListLeft(3, EmptyList())));
+	print_f(print_r(l));
+	puts("");
+
+	puts("TEST MakeListRight");
+	list* j = MakeListRight(1, MakeListRight(2, MakeListRight(3, EmptyList())));
+	print_r(print_f(j));
 }
